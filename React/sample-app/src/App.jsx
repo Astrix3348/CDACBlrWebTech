0c1a700206053aaaa76fac5b3ee7c1d254cb2fa2
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-10-29 12:51:33 +0530

    updated with state management

--- React/sample-app/src/App.jsx
+++ React/sample-app/src/App.jsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Ex01Basic from './Ex01Basic'
-
+import Ex02EventHandling from './Components/Ex02EventHandling'//import the Component
+import Ex03UsingState from './Components/Ex03UsingState'
+import Ex04CalcApp from './Components/Ex04CalcApp'
+import { Ex05CrudApp } from './Components/Ex05CrudApp'
 export default function App() {
   const [count, setCount] = useState(0)
 
@@ -11,7 +14,12 @@ export default function App() {
     <>
      <h1>Welcome to React with Vite!!!!</h1>
      <hr />
-     <Ex01Basic/>
+     {/* <Ex01Basic/>
+     <Ex03UsingState/>
+     <Ex02EventHandling/>
+     <Ex04CalcApp/> 
+     */}
+     <Ex05CrudApp/>
     </>
   )
 }
