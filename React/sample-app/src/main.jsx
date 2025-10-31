088db73ab1a97b9ff127336584c9df9ef5022f1f
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-10-31 11:15:45 +0530

    updated with redux and effect hoots

--- React/sample-app/src/main.jsx
+++ React/sample-app/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/fontawesome-free/css/all.min.css'
+import { Provider } from 'react-redux'
+import { store } from './Components/ReduxExample/store.jsx'
+import { employeeStore } from './Components/ReduxEmployeesExample/employeeStore.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Provider store={employeeStore}> */}
+    <Provider store={employeeStore}>
+
+      <App />
+    </Provider>
   </StrictMode>,
 )
